d208313e99759fceff04f37478e806d90337a365
jorgeguedess <jorge_guedes00@hotmail.com>
2024-09-08 20:47:52 -0300

    feat: automating file creation with plop

---
+++ src/components/MyComponent/MyComponent.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react"
+
+import MyComponent from "."
+
+export default {
+  title: "MyComponent",
+  component: MyComponent,
+  tags: ["autodocs"]
+} as Meta
+
+export const Default: StoryObj = {}
